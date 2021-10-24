444e6d4f0dba1dc2c563d0571054923853cec7dd
xWOYO <wojteka99@interia.pl>
2021-10-24 17:40:32 +0200

    added working cart

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,8 @@ router.get('/', function(req, res, next) {
   }).lean();
 });
 
+var currCart;
+
 router.get('/addtocart/:id', function(req, res, next){
   var productId = req.params.id;
   var cart = new Cart(req.session.cart ? req.session.cart : {qty: 0, item: {}, price: 0});
@@ -30,9 +32,30 @@ router.get('/addtocart/:id', function(req, res, next){
     }
     cart.add(product, product.id);
     req.session.cart = cart;
-    console.log(req.session.cart);
+    // console.log(req.session.cart);
+    currCart = cart
     res.redirect('/')
   });
+});
+
+// router.get('/shoppingcart', function(res, req, next){
+//   if(!currCart){
+
+//     console.log('in');
+//     return res.render('shop/shoppingcart', {products: null});
+//   }
+//   var cart = new Cart(req.session.cart);
+//   console.log(cart);
+//   res.render('shop/shoppingcart', {products: cart.generateArray(), totalPrice: cart.totalPrice})
+// });
+
+router.get('/shoppingcart', function (req, res, next) {
+  if (!req.session.cart) {
+      return res.render('shop/shoppingcart', {products: null});
+  }
+  var cart = new Cart(req.session.cart);
+  // console.log(req.session.cart);
+  res.render('shop/shoppingcart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 module.exports = router;
