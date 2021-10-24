72f295dc10de72f4aab37836cf2a9c647b764065
xWOYO <wojteka99@interia.pl>
2021-10-24 13:36:38 +0200

    added session stored items

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
@@ -31,7 +32,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'secret', resave: false, saveUninitialized: false}));
+app.use(session({
+  secret: 'secret',
+   resave: false, 
+   saveUninitialized: false,
+   store: new MongoStore({mongooseConnection: mongoose.connection}),
+   cookie: {maxAge: 10 * 60 * 1000}
+  }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -39,6 +46,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
   res.locals.logged = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var Product = require('../models/product');
 // var csrf = require('csurf');
 const passport = require('passport');
+var Cart = require('../models/cart');
 
 // var csrfProtection = csrf();
 // router.use(csrfProtection);
@@ -19,5 +20,19 @@ router.get('/', function(req, res, next) {
   }).lean();
 });
 
+router.get('/addtocart/:id', function(req, res, next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {qty: 0, item: {}, price: 0});
+
+  Product.findById(productId, function(err, product){
+    if(err){
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/')
+  });
+});
 
 module.exports = router;
